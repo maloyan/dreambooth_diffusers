da775a5f55c8e2687e2faad105da0efe51d75eb7
Narek Maloyan <narek1110@gmail.com>
2023-03-03 16:23:54 -0400

    different model no nsfw

--- dreambooth_tutorial/predict_light.py
+++ dreambooth_tutorial/predict_light.py
@@ -8,15 +8,17 @@ config = OmegaConf.load(resource_filename(__name__, "../configs/config.yaml"))
 pipe = StableDiffusionPipeline.from_pretrained(
     config.pretrained_model_name_or_path,
     torch_dtype=torch.float16,
+    safety_checker=None
+    
 )
 pipe.unet.load_state_dict(
     torch.load(
-        f"{config.output_dir}/{config.instance_prompt}/checkpoint-400/pytorch_model.bin"
+        f"{config.output_dir}/{config.instance_prompt}/checkpoint-800/pytorch_model.bin"
     )
 )
 pipe = pipe.to("cuda")
 
-prompt = "4k, cinematic, a photo of sks narek"
+prompt = "(extremely detailed CG unity 8k wallpaper), full shot body photo of the most beautiful artwork of narek holding a torch, torn jacket, nostalgia professional majestic oil painting by Ed Blinkey, Atey Ghailan, Studio Ghibli, by Jeremy Mann, Greg Manchess, Antonio Moro, trending on ArtStation, trending on CGSociety, Intricate, High Detail, Sharp focus, dramatic, photorealistic painting art by midjourney and greg rutkowski"
 
 
 image = pipe(prompt).images[0]
